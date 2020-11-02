6743273ac9ec245ae9e0933de7c4629a6fbee04f
aquajack22 <aquajack22@gmail.com>
2020-11-02 15:44:04 +0100

    Update App.java
    Update

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,8 +10,7 @@ public class App
 
     public App() {}
 
-    public static void main(String[] args) {
-        System.out.println(" test jenkins git polling");
+    public static void main(String[] args) {        
         System.out.println(new App().getMessage());
     }
 
